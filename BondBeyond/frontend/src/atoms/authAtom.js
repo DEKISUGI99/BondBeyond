0f0b72caccc8d65c75f4d391b9f7eb28925d3114
DEKISUGI99 <tanishqtank996@gmail.com>
2025-01-19 19:32:55 +0530

    Removed folder from directory

--- BondBeyond/frontend/src/atoms/authAtom.js
+++
@@ -1,6 +0,0 @@
-import {atom} from 'recoil';
-const authScreenAtom = atom({
-    key:'authScreenAtom',
-    default:'login',
-})
-export default authScreenAtom;
--- BondBeyond/frontend/src/atoms/postAtom.js
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-const postsAtom = atom({
-	key: "postsAtom",
-	default: [],
-});
-
-export default postsAtom;
